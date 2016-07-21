6c6094b1a5be9541e6f89d5a18d0bfb0f8c069cd
Abel Ingrand <abel@ingrand.net>
2016-07-21 11:38:06 +0200

    Added background audio to fix #76 and #12

--- xcode-project/pokemon-webspoof/ViewController.h
+++ xcode-project/pokemon-webspoof/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)launchPokemonGo:(id)sender;
+- (IBAction)backgroundAppSwitchChanged:(id)sender;
 
 @end
 
--- xcode-project/pokemon-webspoof/ViewController.m
+++ xcode-project/pokemon-webspoof/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "AVFoundation/AVFoundation.h"
+#import "AudioToolbox/AudioToolbox.h"
 
-@interface ViewController ()
+
+@interface ViewController () <AVAudioPlayerDelegate> {
+    
+    AVAudioPlayer *backgroundAudioPlayer; //Plays silent audio in the background to keep
+}
 
 @end
 
@@ -16,7 +22,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"silent" ofType:@"mp3"]];
+    backgroundAudioPlayer=[backgroundAudioPlayer initWithContentsOfURL:url error:nil];
+    backgroundAudioPlayer.numberOfLoops=-1;
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback withOptions:AVAudioSessionCategoryOptionMixWithOthers error:nil]; //Allows the silent audio to play along side iTunes
+    [[AVAudioSession sharedInstance] setActive:YES error:nil];
+    
+    [backgroundAudioPlayer play];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +38,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)launchPokemonGo:(id)sender {
+    
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"b335b2fc-69dc-472c-9e88-e6c97f84091c-3://"]]) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"b335b2fc-69dc-472c-9e88-e6c97f84091c-3://"]];
+    }
+    
+}
+
+- (IBAction)backgroundAppSwitchChanged:(id)sender {
+    
+    if ([(UISwitch*)sender isOn]) {
+        [backgroundAudioPlayer play];
+    }
+    else{
+        [backgroundAudioPlayer stop];
+    }
+}
 @end
