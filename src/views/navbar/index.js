284ba904b6a667a3d7f4ca58815da328575e73b9
Max Tyler <iam4x@me.com>
2016-09-18 18:14:11 +0200

    refactor: remove server status not useful anymore

--- src/views/navbar/index.js
+++ src/views/navbar/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import ServerStatus from './server-status.js'
 import UserLocationName from './user-location-name.js'
 import GithubStar from './github-star.js'
 
 const Navbar = () =>
   <div className='navbar clearfix'>
-    <ServerStatus />
     <UserLocationName />
     <GithubStar />
   </div>
